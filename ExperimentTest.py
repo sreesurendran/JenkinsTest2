e42d98e7b764e624386214e80da02bef77aab921
sreesurendran <sree.surendran55@gmail.com>
2014-11-03 13:32:25 -0600

    Added test cases

--- ExperimentTest.py
+++ ExperimentTest.py
@@ -7,5 +7,13 @@ class MyTestCase(unittest.TestCase):
     r1 = Experiment.largest([1,1,1]) 
     self.assertEqual(r1, 1)
 
+  def test_t2(self):
+    r1 = Experiment.largest([3,2,1]) 
+    self.assertEqual(r1, 3)
+
+  def test_t3(self):
+    r1 = Experiment.largest([3,2,3]) 
+    self.assertEqual(r1, 3)
+
 if __name__ == '__main__':
   unittest.main()
